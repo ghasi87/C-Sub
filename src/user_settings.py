9fd7eb0b7cfd916ff88f84d4e4eb57899faa5573
mms2024 <mms.gcore2024@gmail.com>
2025-07-18 10:44:39 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -8,6 +8,8 @@ SOURCE_URLS = [
     "https://raw.githubusercontent.com/sorkhi-2/GAFN-ALL/refs/heads/main/configs/proxy_configs.txt",
     "https://raw.githubusercontent.com/GAFN-IVP/GAFN-ALL-II/refs/heads/main/configs/proxy_configs.txt",
     "https://raw.githubusercontent.com/gransa/Temp-Sub/refs/heads/main/configs/proxy_configs.txt",
+    "https://loopycloudcfjiasucdn.xx.kg/api/e5982cccb231b946d617e75481937a93",
+    "https://ne997.688997.xyz/api/v1/client/subscribe?token=2d28dacb3976ca80da6d2fb33274746f",
     "https://raw.githubusercontent.com/Rayan-Config/HUB/refs/heads/main/H-I",
     "https://raw.githubusercontent.com/Rayan-Config/HUB/refs/heads/main/H-II",
     "https://raw.githubusercontent.com/Rayan-Config/HUB/refs/heads/main/H-III",
