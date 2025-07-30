9d77c0bee2589d8b3673c0f86cc7fc5398b37857
mms2024 <mms.gcore2024@gmail.com>
2025-07-30 20:29:56 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -9,6 +9,7 @@ SOURCE_URLS = [
     "https://raw.githubusercontent.com/GAFN-IVP/GAFN-ALL-II/refs/heads/main/configs/proxy_configs.txt",
     "https://raw.githubusercontent.com/gransa/Temp-Sub/refs/heads/main/configs/proxy_configs.txt",
     "https://gw.dport.top/api/v1/client/subscribe?token=12ebb5782047ee015975d54ed85fc776",
+    "ssconf://replic1.rgwebservice.ru/api/conf/0x23653f5d#🟢TakeVPN (@takevpn_bot)",
     "https://raw.githubusercontent.com/Rayan-Config/HUB/refs/heads/main/H-I",
     "https://raw.githubusercontent.com/Rayan-Config/HUB/refs/heads/main/H-II",
     "https://raw.githubusercontent.com/Rayan-Config/HUB/refs/heads/main/H-III",
